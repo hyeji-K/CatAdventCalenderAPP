e9d44609547a9f7242c750fb6ea030a40091971a
Khyeji <khjji0502@gmail.com>
2022-11-25 15:06:04 +0900

    Feat: 네비게이션 컨트롤러 추가

--- CatAdventCalender/CatAdventCalender/ViewController.swift
+++ CatAdventCalender/CatAdventCalender/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "Advent Calender 2022"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "D - 24", style: .plain, target: self, action: nil)
+        navigationController?.navigationBar.tintColor = .black
     }
 
 
