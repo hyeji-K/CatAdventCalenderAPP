c3f5db6edef38183aab0e8f6d527558ca5322dd2
Khyeji <khjji0502@gmail.com>
2022-12-09 01:17:00 +0900

    Feat: 이미지뷰 확대/축소 구현

--- CatAdventCalender/CatAdventCalender/ImageViewController.swift
+++ CatAdventCalender/CatAdventCalender/ImageViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ImageViewController: UIViewController {
     
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
     let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = .customGreenColor
@@ -16,14 +20,17 @@ class ImageViewController: UIViewController {
         imageView.layer.borderWidth = 4
         imageView.layer.masksToBounds = true
         imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
         return imageView
     }()
     var imageName: String
+    var tapGesture = UITapGestureRecognizer()
+    var panGesture = UIPanGestureRecognizer()
+    let zoomOriginScale: CGFloat = 1.0
     
     init(imageName: String) {
         self.imageName = imageName
         super.init(nibName: nil, bundle: nil)
-        
     }
     
     required init?(coder: NSCoder) {
@@ -35,16 +42,71 @@ class ImageViewController: UIViewController {
         
         setupView()
         imageView.image = UIImage(named: imageName)
+        
+        scrollView.delegate = self
+        scrollView.zoomScale = zoomOriginScale
+        scrollView.minimumZoomScale = zoomOriginScale
+        scrollView.maximumZoomScale = zoomOriginScale * 6
+        
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureRecognizerAction))
+        tapGesture.numberOfTapsRequired = 2
+        self.imageView.addGestureRecognizer(tapGesture)
+        
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerHandler))
+        self.view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func panGestureRecognizerHandler(_ panGesture: UIPanGestureRecognizer) {
+        func slideViewVerticallyTo(_ y: CGFloat = 0) {
+            self.view.frame.origin = CGPoint(x: 0, y: y)
+        }
+        
+        switch panGesture.state {
+        case .began, .changed:
+            let translation = panGesture.translation(in: view)
+            let y = max(0, translation.y)
+            slideViewVerticallyTo(y)
+        case .ended:
+            let translation = panGesture.translation(in: view)
+            if translation.y > self.view.frame.size.height * 0.2 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    slideViewVerticallyTo(self.view.frame.size.height)
+                }, completion: { (isCompleted) in
+                    if isCompleted {
+                        self.dismiss(animated: false, completion: nil)
+                    }
+                })
+            } else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    slideViewVerticallyTo()
+                })
+            }
+        default:
+            UIView.animate(withDuration: 0.2, animations: {
+                slideViewVerticallyTo()
+            })
+        }
+    }
+    
+    @objc func tapGestureRecognizerAction(tapGesture: UITapGestureRecognizer) {
+        scrollView.zoomScale = zoomOriginScale
     }
     
     func setupView() {
         self.view.backgroundColor = .black.withAlphaComponent(0.2)
-        self.view.addSubview(imageView)
+        self.view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        
+        scrollView.addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
-        imageView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        imageView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
-        imageView.heightAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: self.scrollView.centerYAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: self.scrollView.heightAnchor).isActive = true
         
         let number = String(imageName.last!)
         
@@ -56,11 +118,10 @@ class ImageViewController: UIViewController {
             imageView.backgroundColor = .customRedColor
         }
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        if let touch = touches.first, touch.view == self.view {
-            self.dismiss(animated: true)
-        }
+}
+
+extension ImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
     }
 }
