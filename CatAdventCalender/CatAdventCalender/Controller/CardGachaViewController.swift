2911af24ef2637c2ff0b9438f468b4022917f393
Khyeji <khjji0502@gmail.com>
2023-08-30 20:29:45 +0900

    Feat: 사진 뽑기 뷰컨트롤러 구현

--- CatAdventCalender/CatAdventCalender/Controller/CardGachaViewController.swift
+++ CatAdventCalender/CatAdventCalender/Controller/CardGachaViewController.swift
@@ -15,7 +15,25 @@ class CardGachaViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        setupView()
+    }
+    
+    private func setupView() {
+        cardImageView.tintColor = .frashGreenColor
+        cardImageView.layer.cornerRadius = 30
+        cardImageView.contentMode = .scaleAspectFit
+        cardImageView.layer.borderColor = UIColor.frashGreenColor.cgColor
+        cardImageView.layer.borderWidth = 1
+        
+        gachaButton.layer.cornerRadius = 10
+        gachaButton.backgroundColor = .frashGreenColor
+        gachaButton.tintColor = .white
+        gachaButton.addTarget(self, action: #selector(gachaButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func gachaButtonTapped(_ sender: UIButton) {
+        print("과연!")
+        cardImageView.image = UIImage(named: "gift4")
     }
 }
