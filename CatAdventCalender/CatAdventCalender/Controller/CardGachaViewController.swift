fa796c3ce2cd6d085c3bbc44a21462e33627ed38
Khyeji <khjji0502@gmail.com>
2023-08-30 20:10:18 +0900

    Feat: 사진 뽑기 화면 storyboard 구성

---
+++ CatAdventCalender/CatAdventCalender/Controller/CardGachaViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CardGachaViewController.swift
+//  CatAdventCalender
+//
+//  Created by heyji on 2023/08/30.
+//
+
+import UIKit
+
+class CardGachaViewController: UIViewController {
+
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var gachaButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
